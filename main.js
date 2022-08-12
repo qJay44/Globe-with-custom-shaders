7507a0fe0aa24137a62e4926049ccfa5779ef612
qJay44 <m70155302@yandex.ru>
2022-08-12 16:43:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
+
+// create a sphere
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({ color: 0xFF0000 })
+)
+
+scene.add(sphere)
+camera.position.z = 10
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+
+animate()
