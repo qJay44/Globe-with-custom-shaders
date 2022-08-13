002a75ed0720b7c56bdf54d1d296fc39c20366a6
qJay44 <m70155302@yandex.ru>
2022-08-13 16:29:50 +0300

    map a texture onto sphere

--- main.js
+++ main.js
@@ -10,15 +10,16 @@ document.body.appendChild(renderer.domElement)
 // create a sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  new THREE.MeshBasicMaterial({ color: 0xFF0000 })
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/globe.jpg') })
 )
 
 scene.add(sphere)
 camera.position.z = 10
 
-function animate() {
-  requestAnimationFrame(animate)
+function animate(frame) {
   renderer.render(scene, camera)
+  
+  requestAnimationFrame(animate)
 }
 
 animate()
