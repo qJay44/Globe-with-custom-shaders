4e4787e28209546b001e6767168c8317fc84fcd3
qJay44 <m70155302@yandex.ru>
2022-08-14 16:24:22 +0300

    sharpen rendering

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
+import { RedFormat } from 'three'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 // create a sphere
@@ -14,7 +16,7 @@ const sphere = new THREE.Mesh(
 )
 
 scene.add(sphere)
-camera.position.z = 10
+camera.position.z = 15
 
 function animate(frame) {
   renderer.render(scene, camera)
