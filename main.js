463bea4447d29d7f59de84618dce312f2df5253c
qJay44 <m70155302@yandex.ru>
2022-08-15 14:58:24 +0300

    import vertex shader

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { RedFormat } from 'three'
+import vertexShader from './shaders/vertex.glsl'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
@@ -12,13 +12,16 @@ document.body.appendChild(renderer.domElement)
 // create a sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('img/globe.jpg') })
+  new THREE.ShaderMaterial({
+    vertexShader,
+    //fragmentShader:  
+  })
 )
 
 scene.add(sphere)
 camera.position.z = 15
 
-function animate(frame) {
+function animate() {
   renderer.render(scene, camera)
   
   requestAnimationFrame(animate)
