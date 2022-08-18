d9a4351ae5b691d3159e00b461f133507b3df0bf
qJay44 <m70155302@yandex.ru>
2022-08-18 17:05:00 +0300

    add texture to fragment shader

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.ShaderMaterial({
     vertexShader,
-    fragmentShader
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./img/globe.jpg')
+      }
+    }
   })
 )
 
--- shaders/fragment.frag
+++ shaders/fragment.frag
@@ -1,3 +1,6 @@
+uniform sampler2D globeTexture;
+varying vec2 vertexUV;
+
 void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(texture2D(globeTexture, vertexUV));
 }
