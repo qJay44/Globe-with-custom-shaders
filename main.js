3766827a0ac0c6d2e297fa457823c418a1b83b65
qJay44 <m70155302@yandex.ru>
2022-08-21 16:22:22 +0300

    add mouse movement interaction

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import globeVertex from './shaders/globe.vert'
 import globeFragment from './shaders/globe.frag'
 import atmosphereVertex from './shaders/atmosphere.vert'
 import atmosphereFragment from './shaders/atmosphere.frag'
+import gsap from 'gsap'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
@@ -26,7 +27,7 @@ const sphere = new THREE.Mesh(
   })
 )
 
-// create a sphere
+// create atmosphere
 const atmosphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
   new THREE.ShaderMaterial({
@@ -39,13 +40,32 @@ const atmosphere = new THREE.Mesh(
 
 atmosphere.scale.set(1.1, 1.1, 1.1);
 
-scene.add(sphere)
+const group = new THREE.Group()
+group.add(sphere)
+
+scene.add(group)
 scene.add(atmosphere)
 camera.position.z = 15
 
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1
+  mouse.y = (event.clientY / innerHeight) * 2 - 1
+})
+
 function animate() {
   renderer.render(scene, camera)
-  sphere.rotation.y += 0.001
+  sphere.rotation.y += 0.002
+  group.rotation.y = mouse.x * 0.5;
+  gsap.to(group.rotation, {
+    x: mouse.y * 0.3,
+    y: mouse.x * 0.5,
+    duration: 2
+  })
   
   requestAnimationFrame(animate)
 }
